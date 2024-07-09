c05b1a63831598229bb32b9e394b2ab80ece00c0
widigie <widigie@gmail.com>
2024-07-09 09:44:15 +0700

    latihan crud node 5th commit update validate js, errorResponse

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bootcampRouter = require('./routes/bootcamp-router');
+const errorHandler = require('./middleware/error');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // set routing
 app.use('/api/bootcamp', bootcampRouter);
+
+// set error middleware
+app.use(errorHandler);
 
 // buat server nya
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
