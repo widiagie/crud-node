bc2f463c1020ed2b888ee7075813026c271245e2
widigie <widigie@gmail.com>
2024-07-12 13:43:54 +0700

    upload commit my react uwexxx

---
+++ frontend-react/src/App.js
@@ -0,0 +1,47 @@
+//import component Bootstrap React
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+//import react router dom
+import { Switch, Route, Link } from "react-router-dom";
+
+//import component Home
+import Home from './pages/Home'
+
+//import component Post Index
+import PostIndex from './pages/posts/Index'
+
+//import component Post Create
+import PostCreate from './pages/posts/Create'
+
+//import component Post Edit
+import PostEdit from './pages/posts/Edit'
+
+function App() {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand to="/">EXPRESS.JS + REACT.JS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/" className="nav-link">HOME</Nav.Link>
+                  <Nav.Link as={Link} to="/posts" className="nav-link">POSTS</Nav.Link>
+                  <Nav.Link as={Link} to="/weather" className="nav-link">WEATHER</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={PostIndex} />
+        <Route exact path="/posts/create" component={PostCreate} />
+        <Route exact path="/posts/edit/:id" component={PostEdit} />
+      </Switch>
+      
+    </div>
+  );
+}
+
+export default App;
